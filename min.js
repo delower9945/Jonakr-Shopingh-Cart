ade22f986caca5dbe5a8a04982ce8da7748811b7
delower9945 <delowerweb99@gmail.com>
2021-06-28 00:41:38 -0700

    Mines Click Button Add  simple

--- min.js
+++ min.js
@@ -9,14 +9,13 @@ document.getElementById('case-increse').addEventListener('click',function(){
 })
 
 
-
-//    document.getElementById('case-increse').addEventListener('click', function () {
-//     const CaseInput = document.getElementById('case-count');
-//     const CaseCount = parseInt(CaseInput.value);
-//     const CaseNewCount = CaseCount +1;
-//     CaseInput.value = CaseNewCount;
-//     const CaseTotal =CaseNewCount*59;
-//     document.getElementById('case-total').innerText = '$'+ CaseTotal;
-
+document.getElementById('case-descrise').addEventListener('click',function(){
+    const CountInput = document.getElementById('case-count');
+    const CaseCount = parseInt(CountInput.value);
+    const CaseNewCount = CaseCount - 1;
+    CountInput.value = CaseNewCount;
     
-//  })
+    const CaseTotal = CaseNewCount * 59;
+    document.getElementById('case-total').innerText ='$'+CaseTotal;
+})
+
